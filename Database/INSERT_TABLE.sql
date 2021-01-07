f295d5c46cc976f66f6b4a9c9bf4c768ad78e3f5
alexrosa336 <1190336@isep.ipp.pt>
2021-01-07 11:46:42 +0000

    Changes to the SQL database structuring

--- Database/INSERT_TABLE.sql
+++ Database/INSERT_TABLE.sql
@@ -7,10 +7,3 @@ INSERT INTO creditcard VALUES(1234567891011122, 09, 2021, 567);
 INSERT INTO Client VALUES(1, 'client1@isep.ipp.pt', 'Joaquim Alberto', 123456789, 'albertojoaquim', 0, 41.15833, -8.62908, 1234567891011121);
 INSERT INTO Client VALUES(2, 'client2@isep.ipp.pt', 'Hernani Carvalho', 134568795, 'carvalhohernani', 0, 41.18200, -8.60119, 1234567891011122);
 
-
-
-select * from client;
-select * from address;
-select * from creditcard;
-
-select * from sailors;
