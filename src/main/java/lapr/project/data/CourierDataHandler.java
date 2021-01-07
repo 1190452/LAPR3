e99e9dd43ecf30f94fa8465cea12dc063f89514a
1190990 <1190990@isep.ipp.pt>
2021-01-07 18:28:43 +0000

    AUCMN1-216 #progress of the UC5 development [Development]

--- src/main/java/lapr/project/data/CourierDataHandler.java
+++ src/main/java/lapr/project/data/CourierDataHandler.java
@@ -1,12 +1,16 @@
 package lapr.project.data;
 
+import jdk.vm.ci.aarch64.AArch64;
 import lapr.project.model.Courier;
 import oracle.jdbc.OracleTypes;
+
 import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CourierDataHandler extends DataHandler{
+public class CourierDataHandler extends DataHandler {
 
     public void addCourier(Courier courier) {
         addCourier(courier.getEmail(), courier.getName(), courier.getWeight(), courier.getNIF(), courier.getNSS(), courier.getMaxWeightCapacity(), courier.getPharmacyID());
@@ -73,5 +77,50 @@ public class CourierDataHandler extends DataHandler{
             e.printStackTrace();
         }
         throw new IllegalArgumentException("No Courier with nif:" + nif);
+    }
+
+    public List<Courier> getCourierList() {
+        /* Objeto "callStmt" para invocar a função "getCourier" armazenada na BD.
+         *
+         * FUNCTION getCourierList(nif INT) RETURN pkgCourier.ref_cursor
+         * PACKAGE pkgCourier AS TYPE ref_cursor IS REF CURSOR; END pkgCourier;
+         */
+        CallableStatement callStmt = null;
+        try {
+            callStmt = getConnection().prepareCall("{ ? = call getCourierList() }");
+
+
+            // Regista o tipo de dados SQL para interpretar o resultado obtido.
+            callStmt.registerOutParameter(1, OracleTypes.CURSOR);
+
+
+            // Executa a invocação da função "getCourierList".
+            callStmt.execute();
+
+            // Guarda o cursor retornado num objeto "ResultSet".
+            ResultSet rSet = (ResultSet) callStmt.getObject(1);
+            List<Courier> courierList = new ArrayList<>();
+
+
+            while (rSet.next()) {
+                int id = rSet.getInt(1);
+                String email = rSet.getString(2);
+                String name = rSet.getString(3);
+                double weight = rSet.getDouble(4);
+                int NIF = rSet.getInt(5);
+                String NSS = rSet.getString(6);
+                double maxCapacity = rSet.getDouble(7);
+                int idPharmacy = rSet.getInt(8);
+
+
+                courierList.add(new Courier(id, email, name, NIF, NSS, maxCapacity, weight, idPharmacy));
+            }
+
+            return courierList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        throw new IllegalArgumentException("No Couriers found");
+
     }
 }
