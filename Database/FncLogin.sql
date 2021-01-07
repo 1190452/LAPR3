6f346666edab9931fbe403cb6770eb81f2210c86
1191461 <1191461@isep.ipp.pt>
2021-01-07 20:24:14 +0000

    AUCMN1 - 212 #start-progress creating functions in the database

---
+++ Database/FncLogin.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION fncLogin(f_email AppUser.email%type, f_password AppUser.password%type) 
+RETURN INTEGER IS 
+    v_count int;
+
+BEGIN
+
+    SELECT COUNT (*) INTO v_count
+    FROM AppUser 
+    WHERE email = f_email AND password = f_password;
+
+    IF v_count = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN 1;
+    END IF;
+    
+END;
+/
+
+--Test
+select fncLogin('client1@isep.ipp.pt', 'ghj') from dual;
