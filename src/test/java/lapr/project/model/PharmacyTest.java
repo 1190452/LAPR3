c85e9babf7e82aa04904c19879ba350268fe7e93
1190336 <1190336@isep.ipp.pt>
2021-01-08 17:31:13 +0000

    AUCMN1-87 #start-progress Enter the data about the pharmacy [Tests]

---
+++ src/test/java/lapr/project/model/PharmacyTest.java
@@ -0,0 +1,56 @@
+package lapr.project.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PharmacyTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getLatitude() {
+    }
+
+    @Test
+    void setLatitude() {
+    }
+
+    @Test
+    void getLongitude() {
+    }
+
+    @Test
+    void setLongitude() {
+    }
+
+    @Test
+    void getEmailAdministrator() {
+    }
+
+    @Test
+    void setEmailAdministrator() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
