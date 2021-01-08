454733827184e3370f7199dce46a12041917c904
1191461 <1191461@isep.ipp.pt>
2021-01-08 16:50:06 +0000

    AUCMN1-212 #comment corrected merged files and added new procedure

--- Database/Functions/getCourierList.sql
+++ Database/Functions/getCourierList.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 create or replace FUNCTION getCourierList
 RETURN SYS_REFCURSOR
 AS
@@ -9,15 +8,4 @@ BEGIN
   RETURN c; 
 END;
 
-=======
-create or replace FUNCTION getCourierList
-RETURN SYS_REFCURSOR
-AS
-  c SYS_REFCURSOR;	
-BEGIN
-  OPEN c FOR 
-  SELECT * FROM Courier; 
-  RETURN c; 
-END;
 
->>>>>>> 0732e975e1b66f7885a9d37bc7b94ebefdcbd217
--- Database/Functions/getScooterList.sql
+++ Database/Functions/getScooterList.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 create or replace FUNCTION getScooterList
 RETURN SYS_REFCURSOR
 AS
@@ -7,14 +6,5 @@ BEGIN
   OPEN c FOR 
   SELECT id, ah_battery, v_battery,enginepower, weight, idpharmacy FROM electricscooter; 
   RETURN c; 
-=======
-create or replace FUNCTION getScooterList
-RETURN SYS_REFCURSOR
-AS
-  c SYS_REFCURSOR;	
-BEGIN
-  OPEN c FOR 
-  SELECT id, ah_battery, v_battery,enginepower, weight, idpharmacy FROM electricscooter; 
-  RETURN c; 
->>>>>>> 0732e975e1b66f7885a9d37bc7b94ebefdcbd217
+
 END;
