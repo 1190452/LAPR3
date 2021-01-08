4aa12f83d6c9486ad61aa185da72c9dd3c417c75
1190990 <1190990@isep.ipp.pt>
2021-01-08 17:17:25 +0000

    AUCMN1-133 #start of tests of UC14 [tests]

---
+++ src/test/java/lapr/project/model/ClientOrderTest.java
@@ -0,0 +1,64 @@
+package lapr.project.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientOrderTest {
+
+    @Test
+    void getFinalPrice() {
+    }
+
+    @Test
+    void setFinalPrice() {
+    }
+
+    @Test
+    void getFinalWeight() {
+    }
+
+    @Test
+    void setFinalWeight() {
+    }
+
+    @Test
+    void getClientId() {
+    }
+
+    @Test
+    void setClientId() {
+    }
+
+    @Test
+    void getOrderId() {
+    }
+
+    @Test
+    void setOrderId() {
+    }
+
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void setDate() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+
+    @Test
+    void setStatus() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getOrder() {
+    }
+}
