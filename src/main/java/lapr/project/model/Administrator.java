12d1d621791659c98aa1a2d5e009d9cdaab17b8b
Jorge Silva <1191458@isep.ipp.pt>
2021-01-09 19:01:56 +0000

    AUCMN1-220

--- src/main/java/lapr/project/model/Administrator.java
+++ src/main/java/lapr/project/model/Administrator.java
@@ -5,8 +5,6 @@ import java.util.Objects;
 
 public class Administrator extends User{
     private String name;
-    private String password;
-    private String role;
 
 
     /**
@@ -28,25 +26,8 @@ public class Administrator extends User{
     }
 
     @Override
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Administrator)) return false;
-        if (!super.equals(o)) return false;
-        Administrator that = (Administrator) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(password, that.password) &&
-                Objects.equals(role, that.role);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, password, role);
+    public void setPassword(String strPassword) {
+        super.setPassword(strPassword);
     }
 
     @Override
