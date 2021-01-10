de1a0b64d2c71068cb41d0e97e38ef66c79ddd1d
alexrosa336 <1190336@isep.ipp.pt>
2021-01-10 17:10:46 +0000

    AUCMN1 - 167 #comment changes to the struct parameters

--- C and Assembly/LAPR3/struct.h
+++ C and Assembly/LAPR3/struct.h
@@ -1,18 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
    typedef struct {
-     int scooterID;
+     char[8] licensePlate;
      int parkID;
      int charging_place_potency;
      int ah_battery;
      int maxBattery;
      int actualBattery;
-	 int year;
-	 int month;
-	 int day;
-	 int hour;
-	 int minute;
-	 int second;
+     int year;
+     int month;
+     int day;
+     int hour;
+     int minute;
+     int second;
    } s1;
 
 #endif
