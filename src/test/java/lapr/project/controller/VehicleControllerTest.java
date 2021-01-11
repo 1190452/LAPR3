f507e6ba465534e1b7fd5304e3ee11fe32266b09
Bruno Pereira <1190452@isep.ipp.pt>
2021-01-11 23:05:53 +0000

    AUCMN1-118 - #Progress Park the Scooter [Tests]

--- src/test/java/lapr/project/controller/VehicleControllerTest.java
+++ src/test/java/lapr/project/controller/VehicleControllerTest.java
@@ -1,58 +1,66 @@
 package lapr.project.controller;
 
+import lapr.project.data.DeliveryHandler;
+import lapr.project.data.ParkHandler;
 import lapr.project.data.VehicleHandler;
-import lapr.project.model.Vehicle;
+import lapr.project.model.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class VehicleControllerTest {
 
     private static VehicleController instance;
 
-    public VehicleControllerTest() {
-
-    }
-
     @BeforeAll
-    static void beforeAll() throws SQLException {
+    public static void setUpClass() {
+
+        DeliveryHandler deliveryHandlerMock = mock(DeliveryHandler.class);
         VehicleHandler vehicleHandlerMock = mock(VehicleHandler.class);
-        Vehicle vehicle = new Vehicle(1,"AH-87-LK",400,350,1,1,500,8.0,5000.0,430,4, 1);
-        when(vehicleHandlerMock.getAllVehicles()).thenReturn(new ArrayList<>());
-        vehicleHandlerMock.addVehicle(vehicle);
+        ParkHandler parkDataHandlerMock = mock(ParkHandler.class);
 
-        instance = new VehicleController(vehicleHandlerMock);
-    }
-
-   @Test
-    void addScooter() throws SQLException {
-        Vehicle vehicle = new Vehicle(1,"AH-87-LK",400,350,1,1,500,8.0,5000.0,430,4, 1);
-        VehicleController vehicleController = mock(VehicleController.class);
-        doNothing().when(vehicleController).addVehicle(vehicle.getLicensePlate(),vehicle.getMaxBattery(),vehicle.getActualBattery(),vehicle.getAh_battery(),vehicle.getV_battery(),vehicle.getEnginePower(),vehicle.getWeight(),vehicle.getIdPharmacy(),vehicle.getTypeVehicle());
-        vehicleController.addVehicle(vehicle.getLicensePlate(),vehicle.getMaxBattery(),vehicle.getActualBattery(),vehicle.getAh_battery(),vehicle.getV_battery(),vehicle.getEnginePower(),vehicle.getWeight(),vehicle.getIdPharmacy(),vehicle.getTypeVehicle());
-
-        verify(vehicleController, times(1)).addVehicle(vehicle.getLicensePlate(),vehicle.getMaxBattery(),vehicle.getActualBattery(),vehicle.getAh_battery(),vehicle.getV_battery(),vehicle.getEnginePower(),vehicle.getWeight(),vehicle.getIdPharmacy(),vehicle.getTypeVehicle());
-        //instance.addScooter(vehicle.getLicencePlate(), vehicle.getMaxBattery(),vehicle.getActualBattery(),vehicle.getAh_battery(),vehicle.getV_battery(),vehicle.getEnginePower(),vehicle.getWeight(), vehicle.getIdPharmacy());
-        //ArrayList<EletricScooter> lst = (ArrayList<EletricScooter>) instance.getEletricScooters();
-        //assertEquals(vehicle, lst.get(0));
+        ArrayList<Vehicle> vehicle = new ArrayList<>();
+        Pharmacy phar = new Pharmacy(5, "ISEP", 2323, 23323, "isep@isep.ipp.pt");
+        Park park = new Park(1, 5,5, 5,5,5,5);
+        Vehicle v = new Vehicle(1, "AB-56-DD", 50, 47, 0, 0, 33, 11,23,56,5, 1);
+        vehicle.add(v);
+        when(vehicleHandlerMock.getAllVehicles()).thenReturn(vehicle);
+        Courier courier = new Courier(1, "Joao");
+        Delivery delivery = new Delivery(45, 333, 23);
+        when(deliveryHandlerMock.getDeliveryByCourierId(courier.getIdCourier())).thenReturn(delivery);
+        when(vehicleHandlerMock.getParkByPharmacyId(phar.getId())).thenReturn(park);
+        when(vehicleHandlerMock.getVehicle("AB-56-DD")).thenReturn(v);
+        when(parkDataHandlerMock.getParkByPharmacyId(5)).thenReturn(park);
+        instance = new VehicleController(vehicleHandlerMock, deliveryHandlerMock, parkDataHandlerMock);
+        VehicleController instance2 = new VehicleController(vehicleHandlerMock);
     }
     @Test
-    void removeScooter() {
+    void getAvailableScooter(){
+        Vehicle expResult = new Vehicle(1, "AB-56-DD", 50, 47, 0, 0, 33, 11,23,56,5, 1);
+        Vehicle result = instance.getAvailableScooter(1);
+        assertEquals(expResult, result);
     }
 
     @Test
+    void parkScooter() throws IOException {
+       boolean expResult = true;
+       boolean result = instance.parkScooter(5, "AB-56-DD");
+       assertEquals(expResult, result);
+    }
+
     void getAvailableVehicles() {
         List<Vehicle> expResult = new ArrayList<>();
+        Vehicle v = new Vehicle("AB-56-DD", 45, 12, 33, 11,23,56,5, 1);
+        expResult.add(v);
         List<Vehicle> result = instance.getVehicles();
 
         assertEquals(expResult, result);
-
-
     }
 }
