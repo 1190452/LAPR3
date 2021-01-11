54ba5fca20a8b0159e08a3db51dcc0ed8501c313
1190990 <1190990@isep.ipp.pt>
2021-01-11 12:47:00 +0000

    AUCMN1-213 #checkout tests [Test]

--- src/test/java/lapr/project/controller/CheckoutControllerTest.java
+++ src/test/java/lapr/project/controller/CheckoutControllerTest.java
@@ -1,12 +1,55 @@
 package lapr.project.controller;
 
+import lapr.project.data.*;
+import lapr.project.model.Cart;
+import lapr.project.model.Client;
+import lapr.project.model.Courier;
+import lapr.project.model.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CheckoutControllerTest {
 
+    private static CheckoutController instance;
+
+    public CheckoutControllerTest(){
+
+    }
+
+    @BeforeAll
+    public static void setUpClass(){
+        ClientDataHandler clientDataHandlerMock = mock(ClientDataHandler.class);
+        ClientOrderHandler clientOrderHandlerMock = mock(ClientOrderHandler.class);
+        InvoiceHandler invoiceHandlerMock = mock(InvoiceHandler.class);
+
+
+        instance = new CheckoutController(clientDataHandlerMock, clientOrderHandlerMock, invoiceHandlerMock);
+    }
+
     @Test
     void checkoutProcess() {
+
+    }
+
+
+    @Test
+    void getUserSession() {
+        User u = new User("xandinho@gmail.com", "qwerty", "CLIENT");
+        UserSession userSessionMock = mock(UserSession.class);
+
+        when(userSessionMock.getUser()).thenReturn(u);
+
+        UserSession.getInstance().setUser(u);
+
+        assertEquals(instance.getUserSession(),UserSession.getInstance().getUser());
     }
 }
