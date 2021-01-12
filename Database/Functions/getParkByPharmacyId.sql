46134676b95fb5d3b201dfe047cb5869118df929
1191461 <1191461@isep.ipp.pt>
2021-01-12 17:00:20 +0000

    AUCMN1-212 #comment added functions

--- Database/Functions/getParkByPharmacyId.sql
+++ Database/Functions/getParkByPharmacyId.sql
@@ -1,10 +1,10 @@
-create or replace FUNCTION getParkByPharmacyId(p_id park.idpharmacy%type)
+create or replace FUNCTION getParkByPharmacyId(p_id park.idpharmacy%type, p_type park.idtypepark%type)
 RETURN SYS_REFCURSOR
 AS
   c SYS_REFCURSOR;	
 BEGIN
   OPEN c FOR 
-  SELECT * FROM park WHERE idpharmacy = p_id; 
+  SELECT * FROM park WHERE idpharmacy = p_id AND idtypepark= p_type; 
   RETURN c; 
 END;
 /
