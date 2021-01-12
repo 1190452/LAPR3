619abc39739238b714841516f2e6d18409886320
Jorge Silva <1191458@isep.ipp.pt>
2021-01-12 14:01:11 +0000

    AUCMN1-213 Update the controller

--- src/main/java/lapr/project/controller/VehicleController.java
+++ src/main/java/lapr/project/controller/VehicleController.java
@@ -139,7 +139,7 @@ public class VehicleController {
         try {
             File myObj = new File(String.format(/*C_and_Assembly\\*/"lock_%4d_%2d_%2d_%2d_%2d_%2d.data",year,month,day,hour,minute,second));    //TODO Verificar a pasta de criação
             if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
+                Logger.getLogger(VehicleController.class.getName()).log(Level.INFO,"File created: " + myObj.getName());
 
                 try (FileWriter myWriter = new FileWriter(myObj)) {
                     myWriter.write(licensePlate+"\n");
@@ -154,32 +154,31 @@ public class VehicleController {
                     myWriter.write(hour+"\n");
                     myWriter.write(minute+"\n");
                     myWriter.write(second+"\n");
-                    
+
                 } catch (IOException ioException) {
                     Logger.getLogger(VehicleController.class.getName()).log(Level.WARNING, ioException.getMessage());
                 }
 
-                    int lines;
-                   try( BufferedReader reader = new BufferedReader(new FileReader(myObj.getPath()))) {
-                        lines = 0;
-                       while (reader.readLine() != null) lines++;
-                   }
+                int lines;
+                try( BufferedReader reader = new BufferedReader(new FileReader(myObj.getPath()))) {
+                    lines = 0;
+                    while (reader.readLine() != null) lines++;
+                }
 
-                    if (lines != 6) {
+                if (lines != 6) {
 
-                    } else {
-                        try {
-                            File flag = new File(String.format(/*C_and_Assembly\\*/"lock_%4d_%2d_%2d_%2d_%2d_%2d.data.flag", year, month, day, hour, minute, second));
-                            if (flag.createNewFile()) {
-                                System.out.println("Flag created: " + flag.getName());
-                            } else {
-                                Logger.getLogger(VehicleController.class.getName()).log(Level.WARNING,"ERROR VehicleController");
-                            }
-                        } catch (IOException e) {
-                            Logger.getLogger(VehicleController.class.getName()).log(Level.WARNING,e.getMessage());
+                } else {
+                    try {
+                        File flag = new File(String.format(/*C_and_Assembly\\*/"lock_%4d_%2d_%2d_%2d_%2d_%2d.data.flag", year, month, day, hour, minute, second));
+                        if (flag.createNewFile()) {
+                            Logger.getLogger(VehicleController.class.getName()).log(Level.INFO,"Flag created: " + flag.getName());
+                        } else {
+                            Logger.getLogger(VehicleController.class.getName()).log(Level.WARNING,"ERROR VehicleController");
                         }
+                    } catch (IOException e) {
+                        Logger.getLogger(VehicleController.class.getName()).log(Level.WARNING,e.getMessage());
                     }
-
+                }
             } else {
                 Logger.getLogger(VehicleController.class.getName()).log(Level.WARNING,"ERROR VehicleController");
             }
