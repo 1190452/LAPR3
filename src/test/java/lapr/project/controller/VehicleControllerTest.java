ba70a5b695bb422c817badc2292ff035ca306ea5
1190990 <1190990@isep.ipp.pt>
2021-01-13 15:14:56 +0000

    AUCMN1-257 #progress [development]

--- src/test/java/lapr/project/controller/VehicleControllerTest.java
+++ src/test/java/lapr/project/controller/VehicleControllerTest.java
@@ -40,7 +40,7 @@ class VehicleControllerTest {
         ArrayList<Vehicle> vehicle = new ArrayList<>();
         Pharmacy phar = new Pharmacy(5, "ISEP", 2323, 23323, "isep@isep.ipp.pt");
         Park park = new Park(1, 5,5, 5,5,5,5, 1);
-        Vehicle v = new Vehicle(1, "AB-56-DD", 50, 47, 0, 0, 33, 11,23,56,5, 1);
+        Vehicle v = new Vehicle(1, "AB-56-DD", 50, 47, 0, 0, 33, 11,23,56,5, 1, 10);
         vehicle.add(v);
         when(vehicleHandlerMock.getAllVehiclesAvaiables()).thenReturn(vehicle);
         when(vehicleHandlerMock.getAllScooterAvaiables(any(Integer.class))).thenReturn(vehicle);
@@ -59,7 +59,7 @@ class VehicleControllerTest {
     }
     @Test
     void getAvailableScooter(){
-        Vehicle expResult = new Vehicle(1, "AB-56-DD", 50, 47, 0, 0, 33, 11,23,56,5, 1);
+        Vehicle expResult = new Vehicle(1, "AB-56-DD", 50, 47, 0, 0, 33, 11,23,56,5, 1, 10);
         Courier c = new Courier(1, "joao@isep.pt", "joao", 1321213, new BigDecimal("37272"), 21, 70, 1);
         Vehicle result = instance.getAvailableScooter(1, c.getEmail());
         assertEquals(expResult, result);
@@ -67,7 +67,7 @@ class VehicleControllerTest {
 
     @Test
     void getAvailableScooter2(){
-        Vehicle expResult = new Vehicle(1, "AB-56-DD", 50, 47, 0, 0, 33, 11,23,56,5, 2);
+        Vehicle expResult = new Vehicle(1, "AB-56-DD", 50, 47, 0, 0, 33, 11,23,56,5, 2, 10);
         Courier c = new Courier(2, "joao@isep.pt", "joao", 1321213, new BigDecimal("37272"), 21, 70, 1);
         Vehicle result = instance.getAvailableScooter(1, c.getEmail());
         assertEquals(expResult, result);
