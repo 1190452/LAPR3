f4ffac1ab3c2ff07205869e1fce21157ed486fbb
Bruno Pereira <1190452@isep.ipp.pt>
2021-01-14 18:21:07 +0000

    resolved merge

--- src/main/java/lapr/project/utils/DoPayment.java
+++ src/main/java/lapr/project/utils/DoPayment.java
@@ -1,13 +1,15 @@
 package lapr.project.utils;
 
 
+import lapr.project.model.Client;
+
 public class DoPayment {
 
     public DoPayment(){
         //metodo para instanciar o payment
     }
 
-    public boolean doesPayment( double price){
+    public boolean doesPayment(Client cl, double price){
         //metodo de simulação de pagamentos
         return (price >= 0);
     }
