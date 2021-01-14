ed9a8b08caf3c56d6d94c1dcaa6c7677ffd1bcec
1191461 <1191461@isep.ipp.pt>
2021-01-14 22:03:12 +0000

    AUCMN1-212 #comment changes to procedure

--- Database/Procedures/prcUpdateStock.sql
+++ Database/Procedures/prcUpdateStock.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE PROCEDURE prcUpdateStockAfterPayment(p_id clientorder.id%type ) IS 
-
+v_idClient int;
+v_finalPrice int;
 BEGIN
 
     FOR c1 IN (SELECT po.idproduct, po.productquantity FROM ProductOrder po 
@@ -10,6 +11,11 @@ BEGIN
         WHERE product.id = c1.idproduct; 
     
     END LOOP;
+    
+        SELECT idClient, finalPrice INTO v_idclient, v_finalPrice
+        FROM ClientOrder WHERE id = p_id;
+    
+        UPDATE client SET credits = credits + 0.1 * v_finalPrice WHERE id = v_idclient;
 
 END;
 /
