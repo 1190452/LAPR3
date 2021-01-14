49b7bb7a178367079c2c7295704c24e8a2439ccb
1191461 <1191461@isep.ipp.pt>
2021-01-14 16:20:02 +0000

    AUCMN1-211 AUCMN1-212 #changes to database + added new functions

--- Database/Procedures/prcUpdateStock.sql
+++ Database/Procedures/prcUpdateStock.sql
@@ -1,18 +1,28 @@
-CREATE OR REPLACE PROCEDURE prcUpdateStock(p_id product.id%type, 
-                                           p_stock product.stock%type) IS 
+CREATE OR REPLACE PROCEDURE prcUpdateStockAfterPayment(p_id clientorder.id%type ) IS 
 
 BEGIN
 
-    UPDATE product SET stock = stock + p_stock WHERE product.id = p_id; 
+    FOR c1 IN (SELECT po.idproduct, po.productquantity FROM ProductOrder po 
+                INNER JOIN ClientOrder co ON po.idOrder = co.id
+                WHERE co.id = p_id) LOOP
+    
+        UPDATE product SET stock = stock - c1.productquantity
+        WHERE product.id = c1.idproduct; 
+    
+    END LOOP;
+
 END;
 /
 
 
-CREATE OR REPLACE PROCEDURE prcUpdateStockR(p_id product.id%type, 
-                                           p_stock product.stock%type) IS 
+CREATE OR REPLACE PROCEDURE prcUpdateStockA(p_idpharmacyA product.idPharmacy%type, p_idpharmacyR product.idPharmacy%type, 
+                                            p_id product.id%type, p_stock product.stock%type) IS 
 
 BEGIN
 
-    UPDATE product SET stock = stock - p_stock WHERE product.id = p_id; 
+    UPDATE product SET stock = stock + p_stock WHERE id = p_id AND idPharmacy = p_idpharmacyA ;
+    
+    UPDATE product SET stock = stock - p_stock WHERE id = p_id AND idPharmacy = p_idpharmacyR ; 
+    
 END;
 /
