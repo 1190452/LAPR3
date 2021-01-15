629476d5ab037deb5351e2664b6471c2bb4c4e69
Jorge Silva <1191458@isep.ipp.pt>
2021-01-15 16:01:44 +0000

    AUCMN1-220 #progress continuing the tests

--- src/main/java/lapr/project/utils/Physics.java
+++ src/main/java/lapr/project/utils/Physics.java
@@ -4,6 +4,8 @@ public class Physics {
     private static final double CONSTANT_GRAVITY=9.80665;
     private static final double CONSTANT_DYNAMIC_FRICTION_COEFFICIENT = 0.80;
     private static final double CONSTANT_AVERAGE_VELOCITY = 5; //m/s
+    private static final double AERODYNAMIC_COEFFICIENT = 0.065;
+    private static final double AIR_DENSITY = 1.225; //to a temperature of 15 degrees
     //private static final double DRONE_WEIGHT = 1.5;
     //private static final double ELECTRIC_SCOOTER_WEIGHT = 80;
 
@@ -26,5 +28,10 @@ public class Physics {
     public double getFrictionalForce(double totalWeight){
         return CONSTANT_DYNAMIC_FRICTION_COEFFICIENT*totalWeight*CONSTANT_GRAVITY;
     }
+
+    public double getAerodynamicDragForce() {
+        return 0;
+    }
+
 
 }
