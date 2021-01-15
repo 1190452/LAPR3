51221b9929a6865f728dd02a3844f7150f278c3c
1191461 <1191461@isep.ipp.pt>
2021-01-15 15:28:29 +0000

    AUCMN1-211 AUCMN1-212 #comment changes to database to include frontalArea + added functions to update data

--- Database/Procedures/updateStatusDelivery.sql
+++ Database/Procedures/updateStatusDelivery.sql
@@ -13,6 +13,14 @@ BEGIN
     SELECT idcourier INTO v_courier
     FROM delivery WHERE id = p_id;
     
+    
+    FOR c1 IN (SELECT co.id FROM Delivery d 
+                INNER JOIN ClientOrder co ON co.idDelivery = d.id
+                WHERE d.id = p_id) LOOP
+    
+        UPDATE ClientOrder SET status = 1 WHERE id = c1.id; 
+    END LOOP;
+    
     UPDATE courier SET status = 0 WHERE id = v_courier;
     
     UPDATE delivery SET status = 1 WHERE id = p_id;
