2d4a616dad4cb92a42e992cc14686ad4f64c3ba1
1191461 <1191461@isep.ipp.pt>
2021-01-17 11:25:35 +0000

    AUCMN1-212 #comment added new function

--- Database/Functions/getDelivery.sql
+++ Database/Functions/getDelivery.sql
@@ -25,5 +25,19 @@ END;
 /
 
 
-
-  
+create or replace FUNCTION getDeliveryByDroneId(p_id vehicle.id%type) 
+RETURN SYS_REFCURSOR
+AS
+  c SYS_REFCURSOR;
+  v_licensePlate vehicle.licensePlate%type;
+BEGIN
+    SELECT licensePlate INTO v_licensePlate
+    FROM vehicle    WHERE id = p_id;
+    
+    OPEN c FOR 
+    SELECT * FROM Delivery WHERE licensePlateVehicle = v_licensePlate
+    ORDER BY id DESC
+    FETCH FIRST ROW ONLY;
+    RETURN c; 
+END;
+/
