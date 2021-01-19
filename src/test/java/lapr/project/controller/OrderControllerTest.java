d80f29f4513810a63e662384946b2e996c8742bd
Jorge Silva <1191458@isep.ipp.pt>
2021-01-19 16:38:06 +0000

    AUCMN1-294 #comment Tests to OrderController

--- src/test/java/lapr/project/controller/OrderControllerTest.java
+++ src/test/java/lapr/project/controller/OrderControllerTest.java
@@ -184,14 +184,14 @@ class OrderControllerTest {
 
     @Test
     void getTotalEnergy() {
-        double expResult = 4080.9539462032817;
+        double expResult = 44.71361155981645;
         double result = instance.getTotalEnergy(15, 200, 1, 5, 10, 30, 40.10, 40.78, -8.33, -8.99);
         assertEquals(expResult, result, 0.1);
     }
 
     @Test
     void getTotalEnergy2() {
-        double expResult = 534426.23;
+        double expResult = 0.4833486696327993;
         double result = instance.getTotalEnergy(0, 12, 2, 1, 0, 0, 40.10, 40.78, 8.33, 8.99);
         assertEquals(expResult, result, 0.1);
     }
