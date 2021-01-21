b16433b48a56348aab1436ab66ad90bc8765460c
1190336 <1190336@isep.ipp.pt>
2021-01-21 17:26:40 +0000

    AUCMN1-305 #close

--- src/test/java/lapr/project/utils/PhysicsTest.java
+++ src/test/java/lapr/project/utils/PhysicsTest.java
@@ -14,93 +14,65 @@ class PhysicsTest {
 
     @Test
     void getNecessaryEnergy1() {
-      double expectedResult = 7.70;
-      double result = ph.getNecessaryEnergy(20000, 50, 1, 5, 10, 3, 90, 0.0020);
-      assertEquals(expectedResult, result, 0.5);
+      double expectedResult = 0.8099;
+      double result = ph.getNecessaryEnergy(20000, 4, 1, 5, 10, 1, 90, 0.0020, 19999);
+      assertEquals(expectedResult, result, 0.05);
     }
 
     @Test
     void getNecessaryEnergy3() {
-        double expectedResult = 6.216;
-        double result = ph.getNecessaryEnergy(20000, 50, 1, 5, 10, 3, 100, 0.0020);
-        assertEquals(expectedResult, result, 0.5);
+        double expectedResult = 0.5088;
+        double result = ph.getNecessaryEnergy(20000, 4, 1, 5, 10, 1, 0, 0.0020, 19999);
+        assertEquals(expectedResult, result, 0.05);
     }
 
     @Test
     void getNecessaryEnergy2() {
-        double expectedResult = 0.1168;
-        double result = ph.getNecessaryEnergy(20000, 6, 2, 1, 10, 3, 90, 0.002);
-        assertEquals(expectedResult, result, 0.5);
+        double expectedResult = 1.900;
+        double result = ph.getNecessaryEnergy(20000, 4, 2, 1, 10, 1, 180, 0.002 ,19999);
+        assertEquals(expectedResult, result, 0.001);
+    }
+
+    @Test
+    void getNecessaryEnergy4() {
+        double expectedResult = 1.8963;
+        double result = ph.getNecessaryEnergy(20000, 4, 2, 1, 10, 1, 0, 0.002, 19999);
+        assertEquals(expectedResult, result, 0.001);
     }
 
     @Test
     void getTimeSpent() {
         double result = ph.getTimeSpent(10000, 4);
-        double expectedResult = 0.694;
+        double expectedResult = 2500.0;
         assertEquals(expectedResult, result, 0.1);
     }
 
 
     @Test
-    void calculateAverageSpeedWithWindDirectionTest1(){
-        double expectedResult = 5;
-        double result = ph.calculateAverageSpeedWithWindDirection(5, 1, 90);
-        assertEquals(expectedResult, result, 0.5);
-    }
-
-    @Test
-    void calculateAverageSpeedWithWindDirectionTest2(){
-        double expectedResult = 4;
-        double result = ph.calculateAverageSpeedWithWindDirection(5, 1, 180);
-        assertEquals(expectedResult, result, 0.5);
-    }
-
-    @Test
-    void calculateAverageSpeedWithWindDirectionTest3(){
-        double expectedResult = 6;
-        double result = ph.calculateAverageSpeedWithWindDirection(5, 1, 0);
-        assertEquals(expectedResult, result, 0.5);
-    }
-
-    @Test
-    void calculateAverageSpeedWithWindDirectionTest4(){
-        double expectedResult = 5;
-        double result = ph.calculateAverageSpeedWithWindDirection(5, 1, 80);
-        assertEquals(expectedResult, result, 0.5);
-    }
-
-    @Test
-    void calculateAverageSpeedWithWindDirectionTest5(){
-        double expectedResult = 5;
-        double result = ph.calculateAverageSpeedWithWindDirection(5, 1, 250);
-        assertEquals(expectedResult, result, 0.5);
-    }
-
-    @Test
     void getAerodynamicDragForce1() {
         double expectedResult = 109.72;
-        double result = ph.getAerodynamicDragForce(5, 1, 4.50);
+        double result = ph.getAerodynamicDragForce(1,5,4.50, 0, 0, 2);
         assertEquals(expectedResult, result, 0.5);
     }
 
     @Test
     void getAerodynamicDragForce2() {
-        double expectedResult = 0.60;
-        double result = ph.getAerodynamicDragForce(1, 2, 4.50);
+        double expectedResult = 2.19;
+        double result = ph.getAerodynamicDragForce(2,1,  4.50, 0, 0, 3);
         assertEquals(expectedResult, result, 0.5);
     }
 
     @Test
     void getAerodynamicDragForce3() {
-        double expectedResult = 0;
-        double result = ph.getAerodynamicDragForce(2, 3, 4.50);
+        double expectedResult = 4.388;
+        double result = ph.getAerodynamicDragForce(2,2, 4.50, 0, 0, 3);
         assertEquals(expectedResult, result, 0.5);
     }
 
     @Test
     void getAerodynamicDragForce4() {
-        double expectedResult = 1.2041;
-        double result = ph.getAerodynamicDragForce(2, 2, 4.50);
+        double expectedResult = 4.388;
+        double result = ph.getAerodynamicDragForce(3,2, 4.50, 0, 0, 3);
         assertEquals(expectedResult, result, 0.5);
     }
 
@@ -129,7 +101,7 @@ class PhysicsTest {
 
     @Test
     void getDroneImpulse() {
-        double expectedResult = 4119.17;
+        double expectedResult = 102.829;
         double result = ph.getDroneImpulse(3, 1, 4);
         assertEquals(expectedResult, result, 0.5);
     }
