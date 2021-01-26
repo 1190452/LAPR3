3b2a7c94cab7006089b5ea42b70ab654922129d7
1191461 <1191461@isep.ipp.pt>
2021-01-26 16:54:28 +0000

    AUCMN1-212 AUCMN1-306 #comment updated

--- Database/Functions/getAllPaths.sql
+++ Database/Functions/getAllPaths.sql
@@ -1,10 +1,10 @@
-create or replace FUNCTION getAllPaths
+create or replace FUNCTION getAllPaths (p_idTypePath path.idTypePath%type)
 RETURN SYS_REFCURSOR
 AS
   c SYS_REFCURSOR;	
 BEGIN
   OPEN c FOR 
-  SELECT * FROM Path; 
+  SELECT * FROM Path WHERE idTypePath = p_idTypePath ; 
   RETURN c; 
 END;
 
