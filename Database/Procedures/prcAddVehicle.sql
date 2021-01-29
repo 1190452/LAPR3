8ac4f05f81cc9dc15556770d8a7276cde60832da
1191461 <1191461@isep.ipp.pt>
2021-01-29 15:21:01 +0000

    AUCMN1-212 #comment updated procedures

--- Database/Procedures/prcAddVehicle.sql
+++ Database/Procedures/prcAddVehicle.sql
@@ -8,13 +8,13 @@ BEGIN
     IF p_idtypevehicle = 1 THEN
 
         INSERT INTO Vehicle(id,licensePlate, maxbattery, actualBattery, ah_Battery, v_Battery, enginePower, weight,frontalArea, idPharmacy, idTypeVehicle ) 
-        VALUES(seq_vehicle.nextval, p_licensePlate, p_maxbattery, p_maxbattery, p_ahBattery, p_vBattery, p_enginePower, 206,0.65, p_idPharmacy, p_idTypeVehicle); 
+        VALUES(seq_vehicle.nextval, p_licensePlate, p_maxbattery, 100, p_ahBattery, p_vBattery, p_enginePower, 206,0.65, p_idPharmacy, p_idTypeVehicle); 
 
         UPDATE park SET actualcapacity = actualcapacity - 1 WHERE idpharmacy = p_idPharmacy AND idtypepark = 1 ;
 
     ELSE
         INSERT INTO Vehicle(id,licensePlate, maxbattery, actualBattery, ah_Battery, v_Battery, enginePower, weight,maxWeightCapacity,frontalArea, idPharmacy, idTypeVehicle ) 
-        VALUES(seq_vehicle.nextval, p_licensePlate, p_maxbattery, p_maxbattery, p_ahBattery, p_vBattery, p_enginePower, 1.5,10,0.005, p_idPharmacy, p_idTypeVehicle); 
+        VALUES(seq_vehicle.nextval, p_licensePlate, p_maxbattery, 100, p_ahBattery, p_vBattery, p_enginePower, 1.5,10,0.005, p_idPharmacy, p_idTypeVehicle); 
 
         UPDATE park SET actualcapacity = actualcapacity - 1 WHERE idpharmacy = p_idPharmacy AND idtypepark = 2 ;
 
