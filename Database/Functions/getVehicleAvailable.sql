f83b35cf2af35311aba95bd88e97e96726b5b302
1191461 <1191461@isep.ipp.pt>
2021-01-30 16:28:23 +0000

    AUCMN1-212 #comment updated database functions

--- Database/Functions/getVehicleAvailable.sql
+++ Database/Functions/getVehicleAvailable.sql
@@ -10,25 +10,26 @@ BEGIN
 END;
 /
 
-create or replace FUNCTION getScooterAvailable(p_id vehicle.idpharmacy%type, p_energy vehicle.actualbattery%type)
+create or replace FUNCTION getScooterAvailable(p_id vehicle.idpharmacy%type, p_energy delivery.necessaryenergy%type)
 RETURN SYS_REFCURSOR
 AS
-  c SYS_REFCURSOR;	
+  c SYS_REFCURSOR;
+  
 BEGIN
-  OPEN c FOR 
-  SELECT * FROM vehicle WHERE idpharmacy = p_id AND idTypeVehicle = 1 AND status=0 AND actualbattery >= p_energy; 
+  OPEN c FOR  
+  SELECT * FROM vehicle WHERE idpharmacy = p_id AND idTypeVehicle = 1 AND status=0 AND actualbattery * maxbattery /100 >= p_energy; 
   RETURN c; 
 
 END;
 /
 
-create or replace FUNCTION getDroneAvailable(p_id vehicle.idpharmacy%type, p_actualbattery vehicle.actualbattery%type)
+create or replace FUNCTION getDroneAvailable(p_id vehicle.idpharmacy%type, p_actualbattery delivery.necessaryenergy%type)
 RETURN SYS_REFCURSOR
 AS
   c SYS_REFCURSOR;	
 BEGIN
   OPEN c FOR 
-  SELECT * FROM vehicle WHERE idpharmacy = p_id AND idTypeVehicle = 2 AND status=0 AND actualbattery >= p_actualbattery ; 
+  SELECT * FROM vehicle WHERE idpharmacy = p_id AND idTypeVehicle = 2 AND status=0 AND actualbattery * maxbattery /100 >= p_actualbattery ; 
   RETURN c; 
   
 END;
