442d5499284402b6f1aa691b1ecebd44bad719f8
1191461 <1191461@isep.ipp.pt>
2021-01-31 15:50:40 +0000

    AUCMN1-212 #comment updated function

--- Database/Functions/fncAddRefillStock.sql
+++ Database/Functions/fncAddRefillStock.sql
@@ -1,28 +1,19 @@
-create or replace function fncAddRefillByDrone( p_necessaryEnergy RefillStock.necessaryenergy%type,
-                                            p_distance RefillStock.distance%type,
-                                            p_weight RefillStock.weight%type, p_licensePlate RefillStock.licensePlateVehicle%type) RETURN INTEGER IS 
 
-BEGIN
-
-    INSERT INTO RefillStock(id, necessaryEnergy, distance, weight, licensePlateVehicle)
-    VALUES (seq_refillstock.nextval, p_necessaryEnergy, p_distance, p_weight, p_licensePlate);
-    
-    RETURN seq_refillstock.currval;
-    
-END;
-/
-
-create or replace function fncAddRefillByScooter( p_necessaryEnergy RefillStock.necessaryenergy%type,
+create or replace function fncAddRefill( p_necessaryEnergy RefillStock.necessaryenergy%type,
                                             p_distance RefillStock.distance%type,
                                             p_weight RefillStock.weight%type,p_licensePlate RefillStock.licensePlateVehicle%type,  
                                             p_idcourier RefillStock.idcourier%type)RETURN INTEGER IS 
 
 BEGIN
+
+IF p_idcourier = 0 THEN
+    INSERT INTO RefillStock(id, necessaryEnergy, distance, weight,licensePlateVehicle)
+    VALUES (seq_refillstock.nextval, p_necessaryEnergy, p_distance, p_weight,p_licensePlate);
+ELSE
     INSERT INTO RefillStock(id, necessaryEnergy, distance, weight,licensePlateVehicle, idcourier)
     VALUES (seq_refillstock.nextval, p_necessaryEnergy, p_distance, p_weight,p_licensePlate, p_idcourier);
-    
+END IF;
     RETURN seq_refillstock.currval;
     
 END;
 /
-
